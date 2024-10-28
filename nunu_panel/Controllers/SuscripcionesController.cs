f18bc0136cc6b6f5160b340729e86decdf19ac0f
Martin Lara <martin.lara@igrtec.com.mx>
2024-10-28 11:06:14 -0600

    fix

--- nunu_panel/Controllers/SuscripcionesController.cs
+++ nunu_panel/Controllers/SuscripcionesController.cs
@@ -1,10 +1,10 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using nunu_panel.Models;
 using RestSharp;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Net;
-using System.Diagnostics;
 
 namespace nunu_panel.Controllers
 {
@@ -19,25 +19,24 @@ namespace nunu_panel.Controllers
 
         public IActionResult Index()
         {
-            var adminName = HttpContext.Session.GetString("AdminName");    
+            var adminName = HttpContext.Session.GetString("AdminName");
             ViewBag.AdminName = adminName;
             var sus = GetSuscripciones();
             return View(sus);
         }
+
         public IActionResult Historial()
         {
             return View();
         }
 
-        private static readonly string apiBaseUrl = "https://api-nunu.igrtecapi.site/api/";
-public List<SuscripcionesModel>? GetSuscripciones()
+        private static readonly string apiBaseUrl = "https://api-nunu.igrtec.net/api/";
+
+        public List<SuscripcionesModel>? GetSuscripciones()
         {
             try
             {
-                var options = new RestSharp.RestClientOptions(apiBaseUrl)
-                {
-                    MaxTimeout = -1
-                };
+                var options = new RestSharp.RestClientOptions(apiBaseUrl) { MaxTimeout = -1 };
 
                 var client = new RestSharp.RestClient(options);
                 var request = new RestSharp.RestRequest("/Proveedores", Method.Get);
@@ -45,7 +44,9 @@ public List<SuscripcionesModel>? GetSuscripciones()
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     string? content = response.Content;
-                    List<SuscripcionesModel>? sus = JsonConvert.DeserializeObject<List<SuscripcionesModel>>(content);
+                    List<SuscripcionesModel>? sus = JsonConvert.DeserializeObject<
+                        List<SuscripcionesModel>
+                    >(content);
                     return sus;
                 }
                 else
@@ -58,27 +59,22 @@ public List<SuscripcionesModel>? GetSuscripciones()
                 return null;
             }
         }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
         public async Task<bool> QuitarSub(int idProveedor)
         {
-    
             try
             {
-                var suscripcionModel = new SuscripcionesModel
-            {
-                suscripcion = false
-            };
-            var options = new RestSharp.RestClientOptions(apiBaseUrl)
-                {
-                    MaxTimeout = -1
-                };
+                var suscripcionModel = new SuscripcionesModel { suscripcion = false };
+                var options = new RestSharp.RestClientOptions(apiBaseUrl) { MaxTimeout = -1 };
                 var client = new RestSharp.RestClient(options);
                 var request = new RestSharp.RestRequest($"/Proveedores/{idProveedor}", Method.Put);
                 request.AddParameter("suscripcion", suscripcionModel.suscripcion);
-                var response = await client.ExecuteAsync(request);                       
+                var response = await client.ExecuteAsync(request);
                 return response.StatusCode == HttpStatusCode.OK;
             }
             catch
@@ -86,11 +82,16 @@ public List<SuscripcionesModel>? GetSuscripciones()
                 return false;
             }
         }
-        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                }
+            );
         }
     }
 }
