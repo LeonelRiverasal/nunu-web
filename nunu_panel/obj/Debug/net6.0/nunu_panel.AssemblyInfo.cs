41f2bf9908a8b09d3761c37d345cf5ca00e9d105
XimenaArmaas <ximena.armas@igrtec.com.mx>
2024-11-12 14:03:17 -0600

    Update fix opcion cerrar sesión

--- nunu_panel/obj/Debug/net6.0/nunu_panel.AssemblyInfo.cs
+++ nunu_panel/obj/Debug/net6.0/nunu_panel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a552311112ecb133bcf21812882cc185355eecea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51df1672dbe31eddabe158bc7f614bd0803c212d")]
 [assembly: System.Reflection.AssemblyProductAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
