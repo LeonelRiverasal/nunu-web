d0fc9cfdd3eb38814c4868524ca9e8b1148044e4
Martin Lara <martin.lara@igrtec.com>
2024-11-14 11:20:24 -0600

    Eliminar usuario Fix

--- nunu_panel/obj/Debug/net6.0/nunu_panel.AssemblyInfo.cs
+++ nunu_panel/obj/Debug/net6.0/nunu_panel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51df1672dbe31eddabe158bc7f614bd0803c212d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f2bf9908a8b09d3761c37d345cf5ca00e9d105")]
 [assembly: System.Reflection.AssemblyProductAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nunu_panel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
